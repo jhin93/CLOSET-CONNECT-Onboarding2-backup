b2b9dd39c089bd62680f356fbc62682b553121e3
jhin93 <scene1993@gmail.com>
2023-08-23 14:14:33 +0900

    data fetching

--- closet_connect_onboarding2/pages/api/hello.js
+++ closet_connect_onboarding2/pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import axios from "axios";
+export default async function handler(req, res) {
+  const creatorList = await axios.get('https://test-connect.api.clo-set.com/api/Social/354/Followers')
+  console.log("----- creatorList(hello.js) : ", creatorList.data)
+  res.status(200).json(creatorList.data)
 }
